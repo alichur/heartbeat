30bfc8bfd12f896a3cf2d2d48c215f0682782c3c
Ali Churcher <churcher.ali@gmail.com>
2023-02-07 17:30:02 +1300

    Hardcoded example API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import { AuthProvider } from "react-oauth2-code-pkce";
 import { authConfig } from "./fitbit-config";
-import { UserInfo } from "./UserInfo";
-
+import UserInfo from "./UserInfo";
+import Home from "./Home";
 function App() {
   return (
     <AuthProvider authConfig={authConfig}>
       <UserInfo />
       <div className="App">
         <h1>Heartbeat</h1>
+        <Home />
       </div>
     </AuthProvider>
   );
