1b7cc5c3dd82a0905883011c7385c107978f3048
Ali Churcher <churcher.ali@gmail.com>
2023-02-09 15:52:30 +1300

    add comment

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 // for use in data with no dates.
-// turns a lonely time string into a date function
+// turns a lonely time string "08:21:45" into a date object with arbitrary date.
+// For use comparing size of times only.
 export function getDateFromOrphanTime(timeString) {
   return Date.parse(`01 Jan 1970 ${timeString} GMT`);
 }
